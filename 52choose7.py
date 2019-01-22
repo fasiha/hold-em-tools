6bc8eea2612d60862e7c84862399548287c7c7fb
Ahmed Fasih <wuzzyview@gmail.com>
2019-01-21 23:09:48 -0500

    Working on analyzing ASCII short-names

--- 52choose7.py
+++ 52choose7.py
@@ -2,18 +2,19 @@
 This script will save 'ncr52_7.txt', a 1.1 GB text file containing all
 133,784,560 (almost 134 million) ways you can choose seven from
 fifty-two, i.e., `nCr(52, 7)`, separated by newlines. The fifty-two
-elements are lowercase and uppercase Latin letters ('a' to 'z', 'A' to
-'Z'), and each line is guaranteed to be sorted, e.g., first line is
-`abcdefg` and the last is `TUVWXYZ`.
+elements are uppercase and lowercase Latin letters ('A' to 'Z', 'a' to
+'z'), and each line is guaranteed to be sorted, e.g., first line is
+`ABCDEFG` and the last is `tuvwxyz`. Because of this, each
+seven-character string is guaranteed to be sorted.
 
 On my laptop (2015-vintage with SSD) and Python 3.6, it takes about a
 minute to run.
 """
 
 import itertools as it
-from string import ascii_letters
+from string import ascii_lowercase, ascii_uppercase
 if __name__ == '__main__':
-    cards = list(ascii_letters)
+    cards = list(ascii_uppercase + ascii_lowercase)
     with open('ncr52_7.txt', 'wt') as fid:
         for comb in it.combinations(cards, 7):
             fid.write(''.join(comb) + '\n')
---
+++ skinnyRank.ts
@@ -0,0 +1,96 @@
+function initCards() {
+  const shorts = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+  const ranks = '0123456789JQK'.split('');
+  const suits = 'cdhs'.split('');
+  let shortToRank: Map<string, string> = new Map([]);
+  let shortToSuit: Map<string, string> = new Map([]);
+  let i = 0;
+  for (let suit of suits) {
+    for (let rank of ranks) {
+      const short = shorts[i++];
+      shortToRank.set(short, rank);
+      shortToSuit.set(short, suit);
+    }
+  }
+  return {shorts, suits, ranks};
+}
+const {shorts, suits, ranks} = initCards();
+
+const loACode = 'a'.charCodeAt(0);
+const upACode = 'A'.charCodeAt(0);
+function shortToNumber(short: string): number {
+  let uppercase = short < 'a';
+  return (short.charCodeAt(0) - (uppercase ? upACode : loACode)) % 13;
+}
+// (0 to 12) || 13 means aces get mapped to 13.
+function shortToNumberAcesHigh(short: string): number { return shortToNumber(short) || 13; }
+function shortToRank(short: string) { return ranks[shortToNumber(short)]; }
+function shortToSuit(short: string): string {
+  return ((short < 'N') && 'c') || ((short <= 'Z') && 'd') || ((short < 'n') && 'h') || 's';
+}
+function validateShort(short: string): boolean { return /^[a-zA-Z]$/.test(short); }
+// Not intended for speed!
+function readableToShort(rank: string, suit: string): string {
+  let rankIdx = ranks.indexOf(rank);
+  let suitIdx = suits.indexOf(suit);
+  if (rankIdx === -1 || suitIdx === -1) { throw new Error('bad readable: ' + [[rank, rankIdx], [suit, suitIdx]]); }
+  return shorts[suitIdx * 13 + rankIdx];
+}
+function sortString(s: string): string { return s.split('').sort().join(''); }
+function range(n: number): number[] { return Array.from(Array(n), (_, n) => n); }
+function flatten<T>(arr: T[][]): T[] { return ([] as T[]).concat(...arr); }
+function initHands(verbose: boolean = false) {
+  const rfRanks = '09JQK'.split('');
+  const royalFlushes = suits.map(suit => rfRanks.map(rank => readableToShort(rank, suit)).join(''));
+  if (verbose) { console.log(royalFlushes); }
+  // not used anywhere since there's that jump between ace and 9.
+
+  const rangeOuter = range(9);
+  const rangeInner = range(5);
+  const straightFlushes = flatten(
+      suits.map(suit => rangeOuter.map(
+                    startRank => rangeInner.map(delta => readableToShort(ranks[startRank + delta], suit)).join(''))));
+  if (verbose) { console.log(straightFlushes); }
+
+  const fours = ranks.map(rank => suits.map(suit => readableToShort(rank, suit)).join(''));
+  if (verbose) { console.log(fours); }
+  return {royalFlushes, straightFlushes};
+}
+const {straightFlushes} = initHands();
+type Hand = string;
+// 1: royal flushes
+function isRoyalFlush(hand: Hand): number {
+  if (hand.length < 5) { return 0; }
+  return ((hand.startsWith('a') && hand.includes('jklm')) || (hand.includes('JKLM') && hand.includes('A')) ||
+          (hand.includes('wxyz') && hand.includes('n')) || (hand.endsWith('WXYZ') && hand.includes('N')))
+             ? 1
+             : 0;
+}
+// 2: straight flushes
+function bestStraightFlush(hand: Hand): number {
+  if (hand.length < 5) { return 0; }
+  let straightFlushesFound: string[] = [];
+  let nhits = 0;
+  let prevCharCode = hand.charCodeAt(0);
+  for (let i = 1; i < hand.length - 5; i++) {
+    let newCharCode = hand.charCodeAt(i);
+    if (prevCharCode + 1 === newCharCode) {
+      if ((++nhits) >= 4 && (shortToSuit(hand[i]) === shortToSuit(hand[i - 4]))) { straightFlushesFound.push(hand[i]); }
+    } else {
+      nhits = 0;
+    }
+    prevCharCode = newCharCode;
+  }
+  if (straightFlushesFound.length > 0) { return Math.max(...straightFlushesFound.map(shortToNumberAcesHigh)); }
+  return 0;
+}
+// 3: four of a kind
+function best4OfAKind(hand: Hand): [number, number] {
+  if (hand.length < 4) { return [0, 0]; }
+  return [0, 0];
+}
+
+if (require.main === module) {
+  console.log(initCards());
+  initHands(true);
+}
