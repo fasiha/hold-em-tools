e263aa7a0c109217f8494b7577ea55fabd13ca65
Ahmed Fasih <wuzzyview@gmail.com>
2019-01-21 20:48:31 -0500

    Python script to make all 52-choose-7 combinations

---
+++ 52choose7.py
@@ -0,0 +1,20 @@
+""" 
+This script will save 'ncr52_7.txt', a 1.1 GB text file containing all
+133,784,560 (almost 134 million) ways you can choose seven from
+fifty-two, i.e., `nCr(52, 7)`, separated by newlines. The fifty-two
+elements are lowercase and uppercase Latin letters ('a' to 'z', 'A' to
+'Z'), and each line is guaranteed to be sorted, e.g., first line is
+`abcdefg` and the last is `TUVWXYZ`.
+
+On my laptop (2015-vintage with SSD) and Python 3.6, it takes about a
+minute to run.
+"""
+
+import itertools as it
+from string import ascii_letters
+if __name__ == '__main__':
+    cards = list(ascii_letters)
+    with open('ncr52_7.txt', 'wt') as fid:
+        for comb in it.combinations(cards, 7):
+            fid.write(''.join(comb) + '\n')
+    print('done')
