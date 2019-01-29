a035e13b7f79e055b1bed3f4d1fde6763c2aac35
Ahmed Fasih <wuzzyview@gmail.com>
2019-01-28 21:19:06 -0500

    Attempting to get SQLite to work

--- histogramBinfile.js
+++ histogramBinfile.js
@@ -1,4 +1,7 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const comb_1 = require("./comb");
@@ -26,24 +29,54 @@ function search(buf, r, key) {
             }
         }
     }
-    return occurrences;
+    return occurrences.slice(1);
 }
 exports.search = search;
+const sqlite3_1 = __importDefault(require("sqlite3"));
 if (require.main === module) {
+    const npocket = 2;
     const r = 5;
     let buf = fs_1.readFileSync('handsScore-' + r + '.bin');
-    let occurrences = Array.from(Array(10 + 1), _ => 0);
-    for (let n = r; n < buf.length; n += (r + 1)) {
-        occurrences[buf[n]]++;
+    {
+        let occurrences = Array.from(Array(10 + 1), _ => 0);
+        for (let n = r; n < buf.length; n += (r + 1)) {
+            occurrences[buf[n]]++;
+        }
+        console.log(occurrences.map((n, i) => [n, i]));
     }
-    console.log(occurrences.map((n, i) => [n, i]));
-    let npocket = 2;
-    let map = new Map();
-    for (let hand of comb_1.combinations(shorts, npocket)) {
-        let thisOccur = search(buf, r, hand.join(''));
-        map.set(hand.join(''), thisOccur);
-        console.log(hand, thisOccur);
-    }
-    fs_1.writeFileSync(`map-npocket${npocket}.json`, JSON.stringify(Array.from(map)));
-    console.log(map);
+    let db = new sqlite3_1.default.Database('holdem-r-' + r + '.sqlite');
+    // db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS holdem (
+      seen TEXT UNIQUE NOT NULL,
+      royalFlush NUMBER,
+      straightFlush NUMBER,
+      fourOak NUMBER,
+      fullHouse NUMBER,
+      flush NUMBER,
+      straight NUMBER,
+      threeOak NUMBER,
+      twoPair NUMBER,
+      pair NUMBER,
+      high NUMBER
+    )`);
+    db.parallelize(() => {
+        let i = 0;
+        for (let hand of comb_1.combinations(shorts, npocket)) {
+            if (++i === 10) {
+                break;
+            }
+            let s = hand.join('');
+            let thisOccur = search(buf, r, s);
+            console.log(hand, thisOccur, 'writing');
+            db.run("INSERT INTO holdem VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [s, ...thisOccur], e => {
+                console.log('inserted');
+                if (e) {
+                    console.error('SQLITE error', e);
+                    throw e;
+                }
+            });
+        }
+    });
+    // });
+    db.close();
 }
--- histogramBinfile.ts
+++ histogramBinfile.ts
@@ -1,5 +1,4 @@
 import {readFileSync, writeFileSync} from 'fs';
-
 import {combinations} from './comb';
 import {initCards} from './skinnyRank'
 
@@ -24,23 +23,52 @@ export function search(buf: Buffer, r: number, key: string): number[] {
       }
     }
   }
-  return occurrences;
+  return occurrences.slice(1);
 }
 
+import sqlite3 from 'sqlite3';
 if (require.main === module) {
+  const npocket = 2;
   const r = 5;
   let buf = readFileSync('handsScore-' + r + '.bin');
-  let occurrences = Array.from(Array(10 + 1), _ => 0);
-  for (let n = r; n < buf.length; n += (r + 1)) { occurrences[buf[n]]++; }
-  console.log(occurrences.map((n, i) => [n, i]));
-
-  let npocket = 2;
-  let map: Map<string, number[]> = new Map();
-  for (let hand of combinations(shorts, npocket)) {
-    let thisOccur = search(buf, r, hand.join(''));
-    map.set(hand.join(''), thisOccur);
-    console.log(hand, thisOccur);
+  {
+    let occurrences = Array.from(Array(10 + 1), _ => 0);
+    for (let n = r; n < buf.length; n += (r + 1)) { occurrences[buf[n]]++; }
+    console.log(occurrences.map((n, i) => [n, i]));
   }
-  writeFileSync(`map-npocket${npocket}.json`, JSON.stringify(Array.from(map)));
-  console.log(map);
+
+  let db = new sqlite3.Database('holdem-r-' + r + '.sqlite');
+  // db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS holdem (
+      seen TEXT UNIQUE NOT NULL,
+      royalFlush NUMBER,
+      straightFlush NUMBER,
+      fourOak NUMBER,
+      fullHouse NUMBER,
+      flush NUMBER,
+      straight NUMBER,
+      threeOak NUMBER,
+      twoPair NUMBER,
+      pair NUMBER,
+      high NUMBER
+    )`);
+
+  db.parallelize(() => {
+    let i = 0;
+    for (let hand of combinations(shorts, npocket)) {
+      if (++i === 10) { break; }
+      let s = hand.join('');
+      let thisOccur = search(buf, r, s);
+      console.log(hand, thisOccur, 'writing');
+      db.run("INSERT INTO holdem VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [s, ...thisOccur], e => {
+        console.log('inserted')
+        if (e) {
+          console.error('SQLITE error', e);
+          throw e;
+        }
+      });
+    }
+  });
+  // });
+  db.close();
 }
