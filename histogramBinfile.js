9e0514669bd59e6c969a19e06f859f9d1b200900
Ahmed Fasih <wuzzyview@gmail.com>
2019-01-29 19:57:45 -0500

    Much smarter algo to search buffer

--- histogramBinfile.js
+++ histogramBinfile.js
@@ -7,42 +7,33 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
-const pify_1 = __importDefault(require("pify"));
 const comb_1 = require("./comb");
 const skinnyRank_1 = require("./skinnyRank");
-var bindAll = require('bind-all');
 const { shorts } = skinnyRank_1.initCards();
-function search(buf, r, key) {
-    let occurrences = Array.from(Array(10 + 1), _ => 0);
-    let keybuf = Buffer.from(key);
-    const keylen = key.length;
-    outer: for (let n = 0; n < buf.length; n += (r + 1)) {
-        let hand = buf.subarray(n, n + r);
-        let targetIdx = 0;
-        if (hand[r - 2] < keybuf[0]) {
-            continue;
+function searchBuf(buf, r, npocket, verbose = false) {
+    let map = new Map();
+    let z = Array.from(Array(10), _ => 0);
+    for (let pocket of comb_1.combinations(shorts, npocket)) {
+        map.set(pocket.join(''), z.slice());
+    }
+    const buflen = buf.length;
+    let i = 0;
+    for (let n = 0; n < buflen; n += (r + 1)) {
+        if (verbose) {
+            if ((++i) % 1e5 === 0) {
+                console.log(i / 1e6);
+            }
         }
-        for (let c of hand) {
-            if (c > keybuf[targetIdx]) {
-                continue outer;
-            }
-            else if (c === keybuf[targetIdx]) {
-                if (++targetIdx === keylen) {
-                    occurrences[buf[n + r]]++;
-                    continue outer;
-                }
-            }
+        const hand = buf.subarray(n, n + r).toString().split('');
+        const rankIdx = buf[n + r] - 1;
+        for (let pocket of comb_1.combinations(hand, npocket)) {
+            map.get(pocket.join(''))[rankIdx]++;
         }
     }
-    return occurrences.slice(1);
+    return map;
 }
-exports.search = search;
-const sqlite3_1 = __importDefault(require("sqlite3"));
 if (require.main === module) {
     (() => __awaiter(this, void 0, void 0, function* () {
         const npocket = 3;
@@ -55,30 +46,7 @@ if (require.main === module) {
             }
             console.log(occurrences.map((n, i) => [n, i]));
         }
-        let db = pify_1.default(bindAll(new sqlite3_1.default.Database(`holdem-r-${r}-n-${npocket}.sqlite`)));
-        yield db.run(`CREATE TABLE IF NOT EXISTS holdem (
-        seen TEXT UNIQUE NOT NULL,
-        royalFlush NUMBER,
-        straightFlush NUMBER,
-        fourOak NUMBER,
-        fullHouse NUMBER,
-        flush NUMBER,
-        straight NUMBER,
-        threeOak NUMBER,
-        twoPair NUMBER,
-        pair NUMBER,
-        high NUMBER
-      )`);
-        for (let hand of comb_1.combinations(shorts, npocket)) {
-            let s = hand.join('');
-            let dbhits = yield db.get(`SELECT COUNT(1) as hits FROM holdem WHERE seen = ?`, [s]);
-            if ('hits' in dbhits && dbhits.hits > 0) {
-                continue;
-            }
-            let thisOccur = search(buf, r, s);
-            console.log(hand, thisOccur);
-            yield db.run("INSERT INTO holdem VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [s, ...thisOccur]);
-        }
-        db.close();
+        let map = searchBuf(buf, r, npocket, true);
+        fs_1.writeFileSync(`map-r-${r}-n-${npocket}.json`, JSON.stringify(Array.from(map)));
     }))();
 }
--- histogramBinfile.ts
+++ histogramBinfile.ts
@@ -1,36 +1,27 @@
 import {readFileSync, writeFileSync} from 'fs';
-import pify from "pify";
 
 import {combinations} from './comb';
 import {initCards} from './skinnyRank'
 
-var bindAll = require('bind-all');
-
 const {shorts} = initCards();
 
-export function search(buf: Buffer, r: number, key: string): number[] {
-  let occurrences = Array.from(Array(10 + 1), _ => 0);
-  let keybuf = Buffer.from(key);
-  const keylen = key.length;
-  outer: for (let n = 0; n < buf.length; n += (r + 1)) {
-    let hand = buf.subarray(n, n + r);
-    let targetIdx = 0;
-    if (hand[r - 2] < keybuf[0]) { continue; }
-    for (let c of hand) {
-      if (c > keybuf[targetIdx]) {
-        continue outer;
-      } else if (c === keybuf[targetIdx]) {
-        if (++targetIdx === keylen) {
-          occurrences[buf[n + r]]++;
-          continue outer;
-        }
-      }
+function searchBuf(buf: Buffer, r: number, npocket: number, verbose: boolean = false) {
+  let map: Map<string, number[]> = new Map();
+  let z = Array.from(Array(10), _ => 0);
+  for (let pocket of combinations(shorts, npocket)) { map.set(pocket.join(''), z.slice()); }
+  const buflen = buf.length;
+  let i = 0;
+  for (let n = 0; n < buflen; n += (r + 1)) {
+    if (verbose) {
+      if ((++i) % 1e5 === 0) { console.log(i / 1e6); }
     }
+    const hand = buf.subarray(n, n + r).toString().split('');
+    const rankIdx = buf[n + r] - 1;
+    for (let pocket of combinations(hand, npocket)) { (map.get(pocket.join('')) as number[])[rankIdx]++; }
   }
-  return occurrences.slice(1);
+  return map;
 }
 
-import sqlite3 from 'sqlite3';
 if (require.main === module) {
   (async () => {
     const npocket = 3;
@@ -42,29 +33,7 @@ if (require.main === module) {
       console.log(occurrences.map((n, i) => [n, i]));
     }
 
-    let db: sqlite3.Database = pify(bindAll(new sqlite3.Database(`holdem-r-${r}-n-${npocket}.sqlite`)));
-    await db.run(`CREATE TABLE IF NOT EXISTS holdem (
-        seen TEXT UNIQUE NOT NULL,
-        royalFlush NUMBER,
-        straightFlush NUMBER,
-        fourOak NUMBER,
-        fullHouse NUMBER,
-        flush NUMBER,
-        straight NUMBER,
-        threeOak NUMBER,
-        twoPair NUMBER,
-        pair NUMBER,
-        high NUMBER
-      )`);
-
-    for (let hand of combinations(shorts, npocket)) {
-      let s = hand.join('');
-      let dbhits: any = await db.get(`SELECT COUNT(1) as hits FROM holdem WHERE seen = ?`, [s]);
-      if ('hits' in dbhits && dbhits.hits > 0) { continue; }
-      let thisOccur = search(buf, r, s);
-      console.log(hand, thisOccur);
-      await db.run("INSERT INTO holdem VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [s, ...thisOccur]);
-    }
-    db.close();
+    let map = searchBuf(buf, r, npocket, true);
+    writeFileSync(`map-r-${r}-n-${npocket}.json`, JSON.stringify(Array.from(map)));
   })();
 }
