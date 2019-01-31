053bc83b21d8986c2fab1cb6eb3896e881b5f74b
Ahmed Fasih <wuzzyview@gmail.com>
2019-01-31 06:52:38 -0500

    fancy footwork

--- histogramBinfile.js
+++ histogramBinfile.js
@@ -7,11 +7,55 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
+const pify_1 = __importDefault(require("pify"));
 const comb_1 = require("./comb");
 const skinnyRank_1 = require("./skinnyRank");
+let appendFile = pify_1.default(fs_1.appendFile);
 const { shorts } = skinnyRank_1.initCards();
+/**
+ * For when `npocket` is big and the str2num map can't store the 10-long numeric arrays
+ */
+function searchBufBigN(buf, r, npocket, verbose = false) {
+    let str2num = new Map();
+    let i = 0;
+    for (let pocket of comb_1.combinations(shorts, npocket)) {
+        str2num.set(pocket.join(''), i * 10);
+        i++;
+        if (verbose && i % 1e4 === 0) {
+            console.log(i / 1e6);
+        }
+    }
+    let hugeOccurBuffer = new Uint32Array(i * 10);
+    i = 0;
+    const buflen = buf.length;
+    // console.log(hugeOccurBuffer.length, str2num.size, str2num.get('yz'));
+    for (let n = 0; n < buflen; n += (r + 1)) {
+        if (verbose && (++i) % 1e5 === 0) {
+            console.log(i / 1e6);
+        }
+        const hand = buf.subarray(n, n + r).toString().split('');
+        const rankIdx = buf[n + r] - 1;
+        for (let pocket of comb_1.combinations(hand, npocket)) {
+            hugeOccurBuffer[rankIdx + str2num.get(pocket.join(''))]++;
+        }
+    }
+    return bigNToStrNumArr(str2num, hugeOccurBuffer);
+}
+function* bigNToStrNumArr(str2num, occurBuf) {
+    let num2str = new Map();
+    for (const [k, v] of str2num) {
+        num2str.set(v, k);
+    }
+    let buflen = occurBuf.length;
+    for (let i = 0; i < buflen; i += 10) {
+        yield [num2str.get(i), Array.from(occurBuf.slice(i, i + 10))];
+    }
+}
 function searchBuf(buf, r, npocket, verbose = false) {
     let map = new Map();
     let z = Array.from(Array(10), _ => 0);
@@ -36,7 +80,6 @@ function searchBuf(buf, r, npocket, verbose = false) {
 }
 if (require.main === module) {
     (() => __awaiter(this, void 0, void 0, function* () {
-        const npocket = 3;
         const r = 7;
         let buf = fs_1.readFileSync('handsScore-' + r + '.bin');
         {
@@ -46,7 +89,20 @@ if (require.main === module) {
             }
             console.log(occurrences.map((n, i) => [n, i]));
         }
-        let map = searchBuf(buf, r, npocket, true);
-        fs_1.writeFileSync(`map-r-${r}-n-${npocket}.json`, JSON.stringify(Array.from(map)));
+        for (let npocket of [6]) {
+            let isNBig = npocket >= 6;
+            // isNBig = true;
+            const fname = `map-r-${r}-n-${npocket}-nbig-${isNBig}.ldjson`;
+            fs_1.writeFileSync(fname, "");
+            let strings = [];
+            for (let kv of (isNBig ? searchBufBigN(buf, r, npocket, true) : searchBuf(buf, r, npocket, true))) {
+                if (strings.length === 1000) {
+                    yield appendFile(fname, strings.join('\n'));
+                    strings = [];
+                }
+                strings.push(JSON.stringify(kv));
+            }
+            yield appendFile(fname, strings.join('\n'));
+        }
     }))();
 }
--- histogramBinfile.ts
+++ histogramBinfile.ts
@@ -1,11 +1,46 @@
-import {readFileSync, writeFileSync} from 'fs';
+import {appendFile as appendFileCb, readFileSync, writeFileSync} from 'fs';
+import pify from 'pify';
 
 import {combinations} from './comb';
 import {initCards} from './skinnyRank'
 
+let appendFile = pify(appendFileCb);
 const {shorts} = initCards();
 
-function searchBuf(buf: Buffer, r: number, npocket: number, verbose: boolean = false) {
+/**
+ * For when `npocket` is big and the str2num map can't store the 10-long numeric arrays
+ */
+function searchBufBigN(buf: Buffer, r: number, npocket: number, verbose: boolean = false) {
+  let str2num: Map<string, number> = new Map();
+  let i = 0;
+  for (let pocket of combinations(shorts, npocket)) {
+    str2num.set(pocket.join(''), i * 10);
+    i++;
+    if (verbose && i % 1e4 === 0) { console.log(i / 1e6); }
+  }
+  let hugeOccurBuffer = new Uint32Array(i * 10);
+  i = 0;
+  const buflen = buf.length;
+  // console.log(hugeOccurBuffer.length, str2num.size, str2num.get('yz'));
+  for (let n = 0; n < buflen; n += (r + 1)) {
+    if (verbose && (++i) % 1e5 === 0) { console.log(i / 1e6); }
+    const hand = buf.subarray(n, n + r).toString().split('');
+    const rankIdx = buf[n + r] - 1;
+    for (let pocket of combinations(hand, npocket)) {
+      hugeOccurBuffer[rankIdx + (str2num.get(pocket.join('')) as number)]++;
+    }
+  }
+  return bigNToStrNumArr(str2num, hugeOccurBuffer);
+}
+
+function* bigNToStrNumArr(str2num: Map<string, number>, occurBuf: Uint32Array): IterableIterator<[string, number[]]> {
+  let num2str: Map<number, string> = new Map();
+  for (const [k, v] of str2num) { num2str.set(v, k); }
+  let buflen = occurBuf.length;
+  for (let i = 0; i < buflen; i += 10) { yield [num2str.get(i) as string, Array.from(occurBuf.slice(i, i + 10))]; }
+}
+
+function searchBuf(buf: Buffer, r: number, npocket: number, verbose: boolean = false): Map<string, number[]> {
   let map: Map<string, number[]> = new Map();
   let z = Array.from(Array(10), _ => 0);
   for (let pocket of combinations(shorts, npocket)) { map.set(pocket.join(''), z.slice()); }
@@ -24,7 +59,6 @@ function searchBuf(buf: Buffer, r: number, npocket: number, verbose: boolean = f
 
 if (require.main === module) {
   (async () => {
-    const npocket = 3;
     const r = 7;
     let buf = readFileSync('handsScore-' + r + '.bin');
     {
@@ -32,8 +66,23 @@ if (require.main === module) {
       for (let n = r; n < buf.length; n += (r + 1)) { occurrences[buf[n]]++; }
       console.log(occurrences.map((n, i) => [n, i]));
     }
+    for (let npocket of [6]) {
+      let isNBig = npocket >= 6;
+      // isNBig = true;
 
-    let map = searchBuf(buf, r, npocket, true);
-    writeFileSync(`map-r-${r}-n-${npocket}.json`, JSON.stringify(Array.from(map)));
+      const fname = `map-r-${r}-n-${npocket}-nbig-${isNBig}.ldjson`;
+      writeFileSync(fname, "");
+
+      let strings: string[] = [];
+
+      for (let kv of (isNBig ? searchBufBigN(buf, r, npocket, true) : searchBuf(buf, r, npocket, true))) {
+        if (strings.length === 1000) {
+          await appendFile(fname, strings.join('\n'));
+          strings = [];
+        }
+        strings.push(JSON.stringify(kv))
+      }
+      await appendFile(fname, strings.join('\n'));
+    }
   })();
 }
